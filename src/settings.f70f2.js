fd5c3a14168a6b54a9e7e6642ad3ecf596cdf4a3
chauncey <realchauncey@gmail.com>
2020-12-17 18:51:58 +0800

    test change file

--- src/settings.f70f2.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/helloworld.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"620a7",main:"ec98a"}};
